38a6a27d5de5a0b12db02251c8c018b1f46c5f61
kaerusapiens <kmy63krr@gmail.com>
2025-02-16 20:17:52 +0900

    new file:   inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,35 @@
+# parent class
+class Person(object):
+
+    # __init__ (コンストラクター作成)
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+        
+    def details(self):
+        print("My name is {}".format(self.name))
+        print("IdNumber: {}".format(self.idnumber))
+    
+# child class
+class Employee(Person):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+        #親クラスのコンストラクタを呼び出す
+        super().__init__(name, idnumber)
+        
+    #override : 親クラスのメソッドを上書きする
+    def details(self):
+        print("My name is {}".format(self.name))
+        print("IdNumber: {}".format(self.idnumber))
+        print("Post: {}".format(self.post))
+
+
+a = Employee('Rahul', 886012, 200000, "Intern")
+
+a.display()
+a.details()
