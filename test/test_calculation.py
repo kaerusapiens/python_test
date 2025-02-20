f2f64fbbf9cc4203df5537b522a41e971f9bc7a0
kaerusapiens <kmy63krr@gmail.com>
2025-02-20 22:13:21 +0900

    test

--- test/test_calculation.py
+++ test/test_calculation.py
@@ -1,19 +1,30 @@
 import unittest
 import calculation
 
-release_name = "lession"
-
-
+release_name = 'dev'
 class TestCal(unittest.TestCase):
-    """実行の順番
-
-    setUp→test_add_num_and_double→tearDown→
-    setUp→test_add_num_and_double_raise→tearDown
     """
+    """
+    def setUp(self):
+        """各テスト実行前に呼び出されるメソッドで、テスト環境を設定する役割"""
+        print('setup')
+        self.cal = calculation.Cal()
+    
+    def tearDown(self):
+        """テスト実行後に後処理を行うメソッドで、setUp() の反対の役割を持つ。"""
+        print('clean up')
+        del self.cal
 
+    #@unittest.skip('skipされました') skipをしたい場合
     def test_add_num_and_double(self):
-        """add_num_and_dobuleメソッドテスト
+        """正常 - 同じ値なのか
         """
-        cal = calculation.Cal()
-        #正常
-        self.assertEqual(cal.add_num_and_double(1,1),4)
+        #cal = calculation.Cal() setUpで定義したため不要
+        self.assertEqual(self.cal.add_num_and_double(1,1),4)
+    @unittest.skipIf(release_name=='dev')
+    def test_add_num_and_double_raise(self):
+        """異常エラー:with文"""
+        #cal = calculation.Cal() setUpで定義したため不要
+        with self.assertRaises(ValueError):
+            self.cal.add_num_and_double('1','1')
+    
